eeb9e9f35e70647f7537bad88efd76396d0afda2
Rajiv Singh <Rajiv Singh@DESKTOP-1VERF3L>
2022-01-20 18:01:30 +0530

    Add project files.

---
+++ WindowsServiceDemo/ProjectInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsServiceDemo
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            //The InitializeComponent method contains the logic which creates and initializes the user interface objects dragged
+            //on the forming surface and provided the Property Grid of Form Designer.
+            InitializeComponent();
+        }
+    }
+}
